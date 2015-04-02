847e87c33bea0c1a13d5aa13aa446433c868e55e
David Lin <dlin128@gmail.com>
2015-04-01 18:20:13 -0700

    fixing again

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name		 "ssh-cookbook"
+name		 "ssh"
 maintainer       "David Lin"
 maintainer_email "dlin128@gmail.com"
 license          "Apache 2.0"
 description      "Configures openssh, adds system users"
-version          "1.4.2"
+version          "1.4.3"
