0fc88df02d6ad5229a801e36904415019ba64e3e
David Lin <dlin128@gmail.com>
2015-04-01 18:21:25 -0700

    fixing again

--- resources/ssh_config.rb
+++
@@ -1,9 +0,0 @@
-actions :add, :add_multiline, :remove
-
-attribute :match,      :kind_of => String,  :name_attribute => true
-attribute :string,     :kind_of => String
-
-def initialize(*args)
-  super
-  @action = :add
-end
